0d2d8149fe135005f4156ee178ec92d0f06b0ae8
kise <kise@kises-MacBook-Air.local>
2025-07-24 20:58:37 +0530

    feat: useState-List-example
    BREAKING CHANGE:

--- src/Title.jsx
+++ src/Title.jsx
@@ -4,12 +4,20 @@ export default function Title() {
     
     const [title, updateTitle] = useState('my title');
     const [inputVal, updateInput] = useState('');
+    const [list, updateList] = useState([]);
 
     return (
         <div>
             <h3>{title}</h3>
             <input type="text" value={inputVal} onChange={e => updateInput(e.target.value)} />
-            <button onClick={() => updateTitle(inputVal)}>Click me</button>
+            <button onClick={() => {
+                updateList([...list, inputVal]);
+                updateInput('');
+                updateTitle(list); // It adds all the string from list together
+                }}>Click me</button>
+            {
+                list.map((item, i) => <div key={i}>{item}</div>)
+            }
         </div>
     )
 }
